9254d7b438e406cdd2b2d511d893a0f89ced570a
Ranish-Shrestha <ranishstha@gmail.com>
2020-07-06 16:15:26 +0545

    partner details completed ranish

--- CMS/Controllers/PartnerController.cs
+++ CMS/Controllers/PartnerController.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Data;
 using System;
 using System.Collections.Generic;
@@ -8,14 +10,20 @@ using DAL;
 using Dapper;
 using BLL.Abstract;
 using DomainModel;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
 
 namespace CMS.Controllers
 {
     public class PartnerController : Controller
     {
+        private readonly IWebHostEnvironment env;
         private readonly IPartner _repo;
-        public PartnerController(IPartner _repo)
+        public PartnerController(IPartner _repo, IWebHostEnvironment env)
         {
+            this.env = env;
             this._repo = _repo;
         }
         public IActionResult Index()
@@ -60,7 +68,7 @@ namespace CMS.Controllers
             }
             return Json(msg);
         }
-        
+
         public JsonResult GetPartnerTitleById(int Id)
         {
             DynamicParameters para = new DynamicParameters();
@@ -86,6 +94,86 @@ namespace CMS.Controllers
                 {
                     pl = "Deleted";
                 }
+            }
+            catch (Exception ex)
+            {
+                pl = ex.Message;
+            }
+            return Json(pl);
+        }
+
+        [HttpPost]
+        public JsonResult PartnerDetails_AddUpdate(DomainModel.Partners mod, IFormFile file)
+        {
+            dynamic msg = string.Empty;
+            try
+            {
+                var filePath = string.Empty;
+                if (file != null)
+                {
+                    var root = Path.Combine(env.WebRootPath, "Images", "Partners");
+                    if (!Directory.Exists(root))
+                    {
+                        Directory.CreateDirectory(root);
+                    }
+
+                    var fileName = Guid.NewGuid().ToString() + "_" + Path.GetFileNameWithoutExtension(file.FileName) + "" + Path.GetExtension(file.FileName);
+
+                    filePath = Path.Combine(root, fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyToAsync(stream);
+                    }
+                    filePath = "https://" + HttpContext.Request.Host.Value + "/Images/Partners/" + fileName;
+                }
+                else
+                {
+                    filePath = "";
+                }
+                DynamicParameters para = new DynamicParameters();
+                para.Add("@Id", mod.Id);
+                para.Add("@PartnerMasterId", mod.PartnerMasterId);
+                para.Add("@Name", mod.Name);
+                para.Add("@ImagePath", filePath);
+                para.Add("@Description", mod.Description);
+                _repo.AddAsync("sp_Partners_AddUpdate", CommandType.StoredProcedure, para);
+                msg = "Success";
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+            return Json(msg);
+        }
+        public JsonResult GetPartnerName(string title)
+        {
+            title = string.IsNullOrEmpty(title) ? "" : title;
+            DynamicParameters para = new DynamicParameters();
+            para.Add("@Name", title);
+            Task<IEnumerable<DomainModel.Partners>> list = _repo.GetListAsync("sp_Partners_GetByPartnerName", CommandType.StoredProcedure, para);
+            List<DomainModel.Partners> pl = list.Result.ToList();
+            return Json(pl);
+        }
+
+        public JsonResult GetPartnerDetailsById(int Id, int pmid)
+        {
+            DynamicParameters para = new DynamicParameters();
+            para.Add("@Id", Id);
+            para.Add("@PartnerMasterId", pmid);
+            Task<DomainModel.Partners> list = _repo.GetFirstAsync("sp_Partners_GetByIdPartnerMasterId", CommandType.StoredProcedure, para);
+            DomainModel.Partners pl = list.Result;
+            return Json(pl);
+        }
+
+        public JsonResult PartnerDetails_Delete(int Id)
+        {
+            dynamic pl = string.Empty;
+            try
+            {
+                DynamicParameters para = new DynamicParameters();
+                para.Add("@Id", Id);
+                _repo.DeleteAsync("sp_Partners_Delete", CommandType.StoredProcedure, para);
+                pl = "Deleted";
             }
             catch (Exception ex)
             {
